7f6892187a23f91b3782cc47f79cd7e75bbe587d
tau <36781951+tndd@users.noreply.github.com>
2024-01-20 00:03:14 +0900

    add method fetch_todo_requests_for_endpoint

--- src/domain/databroker/repository/api.py
+++ src/domain/databroker/repository/api.py
@@ -30,6 +30,13 @@ class DataBrokerApiRepository:
         # TODO
         pass
 
+    def fetch_todo_requests_for_endpoint(self, endpoint: str) -> List[ApiRequest]:
+        """
+        特定エンドポイントの未実行あるいは失敗したAPIのリクエスト一覧を取得する。
+        """
+        todo_api_requests = self.fetch_todo_requests()
+        return [r for r in todo_api_requests if r.endpoint == endpoint]
+
     def fetch_success_results_unmoved(self) -> List[ApiResponse]:
         """
         まだdatasetに未移動の成功したAPIのレスポンス一覧を取得する。
