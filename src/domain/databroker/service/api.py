546489c0f5d7b473968756a86804d5eca3841573
tau <36781951+tndd@users.noreply.github.com>
2024-01-19 23:02:30 +0900

    .

--- src/domain/databroker/service/api.py
+++ src/domain/databroker/service/api.py
@@ -4,14 +4,14 @@ from domain.databroker.model import ApiRequest
 from domain.databroker.repository.api import ApiRepository
 
 
-def request_api(api_request: ApiRequest) -> None:
+def request_api(repo: ApiRepository, api_request: ApiRequest) -> None:
     """
     APIリクエストの内容を実行し、リクエストとその結果の保存を行う。
     """
     pass
 
 
-def multi_requests_todo_api() -> None:
+def multi_requests_todo_api(repo: ApiRepository) -> None:
     """
     未実行あるいは失敗したAPIの実行を行う。
     """
