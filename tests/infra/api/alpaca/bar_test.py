aa861b48f2c180849847fa0c955d34b5383998b8
tau <36781951+tndd@users.noreply.github.com>
2024-01-21 02:08:58 +0900

    add empty test

---
+++ tests/infra/api/alpaca/bar_test.py
@@ -0,0 +1,6 @@
+from infra.api.alpaca.bar import QueryBar, make_request_alpaca_bar
+
+
+def test_make_request_alpaca_bar():
+    # TODO
+    pass
---
+++ tests/infra/api/value_test.py
@@ -0,0 +1,9 @@
+from infra.api.value import ApiQuery
+
+
+def test_api_query():
+    """
+    ApiQueryの変換メソッドが正常に動作しているかのテスト
+    """
+    # TODO
+    pass
