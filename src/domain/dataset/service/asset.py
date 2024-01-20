2d7f99401a57831897efbe51ef50596f3f7c53a2
tau <36781951+tndd@users.noreply.github.com>
2024-01-20 16:13:15 +0900

    add comment & type

--- src/domain/dataset/service/asset.py
+++ src/domain/dataset/service/asset.py
@@ -1,6 +1,12 @@
+from typing import List
+
+from domain.dataset.model.asset import Asset
 from domain.dataset.repository.asset import AssetRepository
 
 
-def get_sector_spdr_fund(rp: AssetRepository):
+def get_sector_spdr_fund(rp: AssetRepository) -> List[Asset]:
+    """
+    ステートストリートのセクターETF一覧を取得する
+    """
     name = 'Sector SPDR Fund'
     return rp.fetch_assets_by_name(name)
