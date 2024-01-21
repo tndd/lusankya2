01d532f3f216711b67881ae6a02f665dcfcd734d
tau <36781951+tndd@users.noreply.github.com>
2024-01-21 15:00:39 +0900

    add test_api_query

--- tests/infra/api/value_test.py
+++ tests/infra/api/value_test.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+
 from infra.api.value import ApiQuery
 
 
@@ -5,5 +7,10 @@ def test_api_query():
     """
     ApiQueryの変換メソッドが正常に動作しているかのテスト
     """
-    # TODO
-    pass
+    @dataclass
+    class TestApiQuery(ApiQuery):
+        f1: int = 1
+        f2: str = 'a'
+
+    test_api_query = TestApiQuery()
+    assert test_api_query.to_params() == {"f1": 1, "f2": "a"}
