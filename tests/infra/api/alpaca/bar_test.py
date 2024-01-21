11e16c2485285abb3058bab30caeb4afeffc3d7d
tau <36781951+tndd@users.noreply.github.com>
2024-01-22 01:01:11 +0900

    add test_make_request_alpaca_bar

--- tests/infra/api/alpaca/bar_test.py
+++ tests/infra/api/alpaca/bar_test.py
@@ -1,6 +1,13 @@
+from domain.databroker.model.api import ApiRequest
 from infra.api.alpaca.bar import QueryBar, make_request_alpaca_bar
 
 
 def test_make_request_alpaca_bar():
-    # TODO
-    pass
+    query_bar = QueryBar(
+        symbol="AAPL",
+        start="2021-01-01",
+        end="2021-01-02",
+        timeframe="1D",
+    )
+    request = make_request_alpaca_bar(query_bar)
+    assert isinstance(request, ApiRequest)
