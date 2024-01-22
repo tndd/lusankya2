74febd659fdf3f5d72f535f498b7dba807e17909
tau <36781951+tndd@users.noreply.github.com>
2024-01-22 16:47:44 +0900

    update query

--- src/infra/psql/query/databroker/insert/api_request.sql
+++ src/infra/psql/query/databroker/insert/api_request.sql
@@ -1,3 +1,4 @@
 INSERT INTO databroker.api_request
 (id, time_stamp, endpoint, params, req_header)
-VALUES(%(id)s, %(time_stamp)s, %(endpoint)s, %(params)s, %(req_header)s);
+VALUES(%(id)s, %(time_stamp)s, %(endpoint)s, %(params)s, %(req_header)s)
+ON CONFLICT (id) DO NOTHING;
--- src/infra/psql/query/databroker/insert/api_response.sql
+++ src/infra/psql/query/databroker/insert/api_response.sql
@@ -1,3 +1,4 @@
 INSERT INTO databroker.api_response
 (time_stamp, api_request_id, status, resp_header, body)
-VALUES(%(time_stamp)s, %(api_request_id)s, %(status)s, %(resp_header)s, %(body)s);
+VALUES(%(time_stamp)s, %(api_request_id)s, %(status)s, %(resp_header)s, %(body)s)
+ON CONFLICT (id) DO NOTHING;
