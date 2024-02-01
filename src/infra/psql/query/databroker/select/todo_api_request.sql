2cd942c804e4d1fe2ffeee0ddb539880fb6619f6
tau <36781951+tndd@users.noreply.github.com>
2024-02-02 03:21:48 +0900

    add sql todo_api_request

--- src/infra/psql/query/databroker/select/todo_api_request.sql
+++ src/infra/psql/query/databroker/select/todo_api_request.sql
@@ -1,37 +1,14 @@
--- 各requestごとの最新responseの日付
-WITH latest_request_id_timestamps AS (
-    SELECT api_request_id, MAX(time_stamp) AS latest_timestamp
-    FROM databroker.api_response
-    GROUP BY api_request_id
-),
--- 各リクエストの最新のresponseの状態
-latest_response as (
-    select
-        rs.id,
-        rs.time_stamp,
-        rs.api_request_id,
-        rs.status,
-        rs.resp_header,
-        rs.body
-    FROM databroker.api_response rs
-    JOIN latest_request_id_timestamps lrt
-    on rs."time_stamp" = lrt.latest_timestamp
-)
--- 各リクエストの最新の結果。
--- 表示されるのは失敗あるいは未実行のもののみ。
 select
-    rq.id,
-    rq."time_stamp" as timestamp_request,
-    rq.endpoint,
-    rq.params,
-    rq.req_header,
-    lr.id as response_id,
-    lr.time_stamp  as "timestamp_response",
-    lr.status,
-    lr.resp_header,
-    lr.body
-from databroker.api_request rq
-left join latest_response lr
-on rq.id = lr.api_request_id
-where lr.status is null
-    or lr.status <> 200;
+    id,
+    timestamp_request,
+    endpoint,
+    params,
+    req_header,
+    response_id,
+    timestamp_response,
+    status,
+    resp_header,
+    body
+from databroker.view_latest_api_result v
+where v.status is null
+    or v.status <> 200;
