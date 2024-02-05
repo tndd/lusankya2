73bed4ffb09cc96ca03be1acd9d59a89d2810149
tau <36781951+tndd@users.noreply.github.com>
2024-02-05 19:36:27 +0900

    add conftest setup_session

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,23 +6,35 @@ from dotenv import load_dotenv
 from domain.databroker.repository.api import DataBrokerApiRepository
 from domain.dataset.repository.asset import AssetRepository
 from infra.db.psql import PsqlClient
+from infra.service.migration import migration
 
-load_dotenv()
+
+@pytest.fixture(scope="session", autouse=True)
+def setup_session():
+    # 環境変数の読み込み
+    load_dotenv()
+    # 環境構築のためのマイグレーション実行
+    cli = _make_psql_client()
+    migration(cli)
 
 
 @pytest.fixture
-def psql_client() -> PsqlClient:
-    url = getenv('PSQL_URL_TEST')
-    if url is None:
-        raise ValueError("Environment variable PSQL_URL_TEST is not set")
-    return PsqlClient(url=url)
+def psql_client():
+    yield _make_psql_client()
 
 
 @pytest.fixture
 def asset_repository():
-    return AssetRepository()
+    yield AssetRepository()
 
 
 @pytest.fixture
-def databroker_api_repository(psql_client) -> DataBrokerApiRepository:
-    return DataBrokerApiRepository(psql_client)
+def databroker_api_repository(psql_client):
+    yield DataBrokerApiRepository(psql_client)
+
+
+def _make_psql_client() -> PsqlClient:
+    url = getenv('PSQL_URL_TEST')
+    if url is None:
+        raise ValueError("Environment variable PSQL_URL_TEST is not set")
+    return PsqlClient(url=url)
