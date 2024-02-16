fc785000bb4a0d5b81ebb4b26e3b60cfe671a380
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-02-16 15:49:42 +0900

    ApiResponseBodyMetadataについての説明を追加。

--- src/domain/databroker/model/api.py
+++ src/domain/databroker/model/api.py
@@ -36,11 +36,24 @@ class ApiResult:
 
 
 @dataclass
-class ApiResultMetdata:
+class ApiResponseBodyMetadata:
     """
-    ApiResponse（あるいはApiResult）のbodyには非常に大きなデータが格納されていることがある。
-    そのため、ApiResponseのbodyにはデータのメタデータのみを格納し、
-    bodyの情報をこのクラスの情報をもとに適宜引き出せるようにする。
+    概要:
+        ApiResponse（あるいはApiResult）のbodyには非常に大きなデータが格納されていることがある。
+        そのため、ApiResponseのbodyにはデータのメタデータのみを格納し、
+        bodyの情報をこのクラスの情報をもとに適宜引き出せるようにする。
+
+    要素:
+        response_id:
+            絶対必要。
+            このidを元に適宜bodyをDBから取り出す。
+        endpoint: 
+            bodyを取り出しても、それ単体ではどのような加工を行なって分析用DBに移動すればいいかが分からない。
+            そのためendpointから内容を判別する必要がある。
+        parameter:
+            現状では用途は無い。だがパラメータによってレスポンスの内容が大幅に変わるということもあり得るため、
+            一応保持しておく。
     """
     endpoint: str
+    parameter: dict
     response_id: str
