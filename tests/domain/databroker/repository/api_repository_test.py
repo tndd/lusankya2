45fe0760cc5afe46e6b53b0267f1ac0a53b38a03
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-02-16 21:58:17 +0900

    test_store_requestの雛形実装

---
+++ tests/domain/databroker/repository/api_repository_test.py
@@ -0,0 +1,9 @@
+from domain.databroker.repository.api import DataBrokerApiRepository
+
+
+def test_store_request(databroker_api_repository):
+    # api_requestのテーブルを初期化
+    # api_requestを作成
+    # api_requestを保存
+    # api_requestが保存されたかの確認
+    pass
