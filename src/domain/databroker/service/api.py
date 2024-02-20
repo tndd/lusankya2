5a55c85682f1eb5182b729f2f94eea9c4f9c8168
tau_mbp2018 <36781951+tndd@users.noreply.github.com>
2024-02-20 15:47:45 +0900

    multi_requests_apiとparallel_requests_apiの実装
    parallel_requestsはmulti_requestsと役割が被っている。
    可能な限りはmultiの方を使って、どうしても高速化させねばならない場合のみparallelを使うという運用になりそうだ。

--- src/domain/databroker/service/api.py
+++ src/domain/databroker/service/api.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ProcessPoolExecutor
 from typing import List
 
 from requests import get
@@ -26,13 +27,28 @@ def request_api(api_request: ApiRequest) -> ApiResponse:
 def multi_requests_api(
         repo: DataBrokerApiRepository,
         api_requests: List[ApiRequest],
-        n_max_worker: int = 8
+    ) -> None:
+    """
+    複数のAPIリクエストの内容を実行し、リクエストとその結果の保存を行う。
+    """
+    for req in api_requests:
+        res = request_api(req)
+        repo.store_request_and_response(req, res)
+
+
+def parallel_requests_api(
+        repo: DataBrokerApiRepository,
+        api_requests: List[ApiRequest],
+        n_max_worker: int = 4
     ) -> None:
     """
     並列で複数のAPIリクエストの内容を実行し、リクエストとその結果の保存を行う。
     """
-    # TODO
-    pass
+    # api_requestsをスレッド数に応じて分割
+    with ProcessPoolExecutor(max_workers=n_max_worker) as executor:
+        for i in range(n_max_worker):
+            chunk_requests = api_requests[i::n_max_worker]
+            executor.submit(multi_requests_api, repo, chunk_requests)
 
 
 def multi_requests_todo_api(repo: DataBrokerApiRepository) -> None:
