979fccd7a8f51e064297aad13045be92f769b6a6
tau <36781951+tndd@users.noreply.github.com>
2024-02-23 20:43:34 +0900

    QueryBarに直接変換メソッドを実装

--- src/infra/api/alpaca/bar.py
+++ src/infra/api/alpaca/bar.py
@@ -2,14 +2,11 @@ from dataclasses import dataclass
 from typing import Optional
 
 from domain.databroker.model.api import ApiRequest
-from infra.api.alpaca.common import HEADER
-from infra.api.model import ApiQuery
-
-ENDPOINT = 'https://data.alpaca.markets/v2/stocks/bars'
+from infra.api.alpaca.common import APCA_ENDPOINT, APCA_HEADER
 
 
 @dataclass
-class QueryBar(ApiQuery):
+class QueryBar:
     symbol: str
     timeframe: str
     start: str
@@ -22,13 +19,34 @@ class QueryBar(ApiQuery):
     page_token: Optional[str] = None
     sort: str = 'asc'
 
+    def to_params(self) -> dict:
+        """
+        要素をApiリクエストのためのパラメータに変換する。
+
+        symbolについてはエンドポイント部分に直接含まれるので、
+        パラメータには含めない。
+        """
+        params = {
+            'timeframe': self.timeframe,
+            'start': self.start,
+            'end': self.end,
+            'limit': self.limit,
+            'adjustment': self.adjustment,
+            'asof': self.asof,
+            'feed': self.feed,
+            'currency': self.currency,
+            'page_token': self.page_token,
+            'sort': self.sort,
+        }
+        return {k: v for k, v in params.items() if v is not None}
+
 
 def make_request_alpaca_bar(query: QueryBar) -> ApiRequest:
     """
     クエリの情報を元にAPIリクエストを作成する。
     """
     return ApiRequest(
-        endpoint=ENDPOINT,
-        header=HEADER,
+        endpoint=APCA_ENDPOINT['bar'].format(symbol=query.symbol),
+        header=APCA_HEADER,
         parameter=query.to_params()
     )
