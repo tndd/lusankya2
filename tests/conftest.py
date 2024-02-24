1836209f40e0804bb2b6fab5af312f3ee7d4b9d4
tau <36781951+tndd@users.noreply.github.com>
2024-02-24 21:47:19 +0900

    ファイル分割の廃止
    なるべくディレクトリ構成をシンプルに保ちたいという欲求からの変更。
    それに今後もこのファイルの複雑性が大幅に上がることも想定していない。なら大丈夫だろう。

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,10 @@
-from os import getenv
-
 import pytest
 from dotenv import load_dotenv
 
 from domain.databroker.repository.api import DataBrokerApiRepository
 from domain.dataset.repository.asset import AssetRepository
-from infra.db.psql import PsqlClient
 from infra.service.migration import migrate
-from tests._test_service.clear_table import clear_tables
+from tests.utils import clear_tables, make_test_psql_client
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -15,7 +12,7 @@ def setup_session():
     # 環境変数の読み込み
     load_dotenv()
     # 環境構築のためのマイグレーション実行
-    cli_db = _make_test_psql_client()
+    cli_db = make_test_psql_client()
     migrate(cli_db)
     # 前回のデータが残存している可能性があるため初期化する
     clear_tables(cli_db)
@@ -23,7 +20,7 @@ def setup_session():
 
 @pytest.fixture
 def psql_client():
-    psql_cli = _make_test_psql_client()
+    psql_cli = make_test_psql_client()
     yield psql_cli
     # テスト終了毎にテーブルを初期化する
     clear_tables(psql_cli)
@@ -37,10 +34,3 @@ def asset_repository():
 @pytest.fixture
 def databroker_api_repository(psql_client):
     yield DataBrokerApiRepository(psql_client)
-
-
-def _make_test_psql_client() -> PsqlClient:
-    url = getenv('PSQL_URL_TEST')
-    if url is None:
-        raise ValueError("Environment variable PSQL_URL_TEST is not set")
-    return PsqlClient(url=url)
