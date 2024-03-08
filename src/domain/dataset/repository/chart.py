34450f1a7b1c0a42f1e3833377d37c4585c2b3e4
tau <36781951+tndd@users.noreply.github.com>
2024-03-08 18:36:47 +0900

    tmp store_chartの実装
    なぜかdbに情報が保存されない

--- src/domain/dataset/repository/chart.py
+++ src/domain/dataset/repository/chart.py
@@ -2,14 +2,28 @@ from dataclasses import dataclass
 
 from domain.dataset.model.chart import Adjustment, Chart, Timeframe
 from infra.db.psql import PsqlClient
+from infra.query.alpaca.insert import get_query_insert_bar
 
 
 @dataclass
 class ChartRepository:
     db_cli: PsqlClient
 
-    def store_chart(self, chart: Chart) -> None:
-        pass
+    def store_chart(
+        self,
+        chart: Chart
+    ) -> None:
+        """
+        Chartモデルを保存する。
+
+        Memo:
+            parallel_modeが有効である場合、並列でinsertが実行される。
+            デフォルトではTrueとする。
+        """
+        # BUG: 情報が保存されない
+        query = get_query_insert_bar()
+        bars_params = chart.to_parameter()
+        self.db_cli.executemany(query, bars_params, parallel_mode=True)
 
 
     def fetch_chart(
--- tests/domain/dataset/repository/chart_repository_test.py
+++ tests/domain/dataset/repository/chart_repository_test.py
@@ -1,8 +1,21 @@
 from domain.dataset.repository.chart import ChartRepository
+from tests.tests_service.factory.domain.dataset.chart import factory_chart
 
 
 def test_store_chart(chart_repository: ChartRepository):
-    pass
+    chart = factory_chart()
+    chart_repository.store_chart(chart)
+    # 保存されたデータの確認
+    chart_repository.db_cli.execute(
+        """
+        INSERT INTO alpaca.bar ("time_stamp", timeframe, symbol, adjustment, "open", high, low, "close", trade_count, volume, vwap) VALUES('2024-02-21T05:00:01+00:00', '1M', 'AAPL', 'raw', 0, 0, 0, 0, 0, 0, 0);
+        """
+    )
+    r = chart_repository.db_cli.execute(
+        'select count(*) from alpaca.bar;'
+    )
+    # 注意: factoryの実装に依存したテスト
+    assert r[0] == 3
 
 
 def test_fetch_chart(chart_repository: ChartRepository):
