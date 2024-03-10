e1e7ad2ebfb399a3a29e199b453c46d24fe14aa8
tau <36781951+tndd@users.noreply.github.com>
2024-03-10 15:54:28 +0900

    move_chart_data_from_broker_to_datasetの雛形実装

---
+++ src/domain/dataset/service/chart.py
@@ -0,0 +1,11 @@
+def move_chart_data_from_broker_to_dataset():
+    """
+    databroker内に存在する未移動のchartデータをdatasetに移動する。
+
+    工程:
+        1. databrokerから対象かつ未移動のchartデータを取得
+        2. 各データをapi生形式からdatasetの形式に変換
+        3. datasetに保存
+        4. 移動済みのdatabroker内のbodyデータを削除する
+    """
+    pass
---
+++ tests/domain/dataset/service/chart_service_test.py
@@ -0,0 +1,12 @@
+from domain.dataset.service.chart import move_chart_data_from_broker_to_dataset
+
+
+def test_move_chart_data_from_broker_to_dataset(databroker_api_repository, chart_repository):
+    """
+    TODO: テスト概要
+        1. databrokerにいくつかのデータをセット
+        2. move_chart_data_from_broker_to_datasetの実行
+        3. 対象のデータのみが移動されたかを確認
+        4. 移動されたデータが削除されていることを確認
+    """
+    pass
