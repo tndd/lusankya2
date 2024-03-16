8c6c2786909b1294ebdc42402ecae6a51ddfc010
tau <36781951+tndd@users.noreply.github.com>
2024-03-17 02:02:42 +0900

    test_factory_api_requestの実装
    簡易的なテスト。
    中身はデバッグモードで確認済み。

--- tests/factory/domain/databroker/api_factory_test.py
+++ tests/factory/domain/databroker/api_factory_test.py
@@ -1,7 +1,13 @@
 from domain.databroker.model.api import ApiRequest
 from infra.api.alpaca.bar import Adjustment_Q, Timeframe_Q
-from tests.tests_service.factory.domain.databroker.api import \
-    factory_alpaca_bar_api_request
+from tests.tests_service.factory.domain.databroker.api import (
+    factory_alpaca_bar_api_request, factory_api_request)
+
+
+def test_factory_api_request():
+    api_request = factory_api_request()
+    assert isinstance(api_request, ApiRequest)
+    
 
 
 def test_factory_alpaca_bar_api_request():
